9f3d0fa49c9cfce64f620b0525edf15c7fb4a598
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2014-06-08 22:44:46 +0200

    Add more requires, add pickup delay timer, fire generic event

--- ShutUpMeg.lua
+++ ShutUpMeg.lua
@@ -4,6 +4,8 @@ Copyright (c) Gnarfoz. All rights reserved
 Find me on IRC at #wildstaruidev @ freenode
 ]]--
  
+require "Apollo"
+require "ApolloTimer"
 require "DatacubeLib"
  
 local ShutUpMeg = {} 
@@ -14,13 +16,22 @@ end
  
 
 function ShutUpMeg:OnLoad()
-	Apollo.RegisterEventHandler("DatacubeUpdated", "ShutUp", self)
+	-- Timer to wait out the 'pick up' sound effect, according to HUDAlerts\HUDAlerts.lua:324
+	local delay = 3.375 + 0.025
+	self.PickUpDelayTimer = ApolloTimer.Create(delay, false, "ShutUp", self)
+	self.PickUpDelayTimer:Stop()
+	
+	Apollo.RegisterEventHandler("DatacubeUpdated", "OnEvent", self)
+end
+
+function ShutUpMeg:OnEvent()
+	Print("Im Eventhandler")
+	self.PickUpDelayTimer:Start()
 end
 
 function ShutUpMeg:ShutUp()
 	DatacubeLib.StopDatacubeSound()
+	Event_FireGenericEvent("GenericEvent_Collections_StopDatacube")
 end
 
--- Pseudo-OO instantiation
-local ShutUpMegInst = ShutUpMeg:new()
-ShutUpMegInst:Init()
+ShutUpMeg:Init()
