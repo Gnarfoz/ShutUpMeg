fb8f96f45a5f50751443ea11d1b5e44dafd0cce0
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2014-06-09 02:12:10 +0200

    Remove debug prints

--- ShutUpMeg.lua
+++ ShutUpMeg.lua
@@ -11,7 +11,7 @@ require "DatacubeLib"
 local ShutUpMeg = {} 
  
 function ShutUpMeg:Init()
-    Apollo.RegisterAddon(self, false)
+	Apollo.RegisterAddon(self, false)
 end
  
 
@@ -20,20 +20,16 @@ function ShutUpMeg:OnLoad()
 end
 
 function ShutUpMeg:OnEvent()
-	Print("Im Eventhandler")
 	if not self.PickUpDelayTimer then
 		-- Timer to wait out the 'pick up' sound effect, according to HUDAlerts\HUDAlerts.lua:324
 		local delay = 3.375 + 0.001
 		self.PickUpDelayTimer = ApolloTimer.Create(delay, false, "ShutUp", self)
-		Print("Timer erzeugt + gestartet")
 	else
 		self.PickUpDelayTimer:Start()
-		Print("Timer gestartet")
 	end	
 end
 
 function ShutUpMeg:ShutUp()
-	Print("ShutUp-Funktion aufgerufen")
 	DatacubeLib.StopDatacubeSound()
 	Event_FireGenericEvent("GenericEvent_Collections_StopDatacube")
 end
